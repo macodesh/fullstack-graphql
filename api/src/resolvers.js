735209c6eccd28744d07278c8f0e0c621305803f
Marlon Couto <devpatola@proton.me>
2023-08-03 18:43:26 -0300

    Adiciona resolução de exercícios

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,23 +1,30 @@
 /**
  * Here are your Resolvers for your Schema. They must match
- * the type definitions in your scheama
+ * the type definitions in your schema
  */
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input);
+    },
+    pet(_, { input }, ctx) {
+      return ctx.models.Pet.findOne(input);
     }
   },
-  User: {
-    
-  }
-}
+  Mutation: {
+    newPet(_, { input }, ctx) {
+      return ctx.models.Pet.create(input);
+    }
+  },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300';
+  //   }
+  // },
+  // User: {
+
+  // }
+};
--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,41 @@
-const { gql } = require('apollo-server')
+const { gql } = require('apollo-server');
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
 
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  input PetInput {
+    id: ID
+    createdAt: String
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput!): Pet!
+  }
+
+  input NewPetInput {
+    name: String!
+    type: String!
+  }
+
+  type Mutation {
+    newPet(input: NewPetInput!): Pet!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
